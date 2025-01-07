7c14be80e12d3907ba370dbda4c26962b98542ae
azhaannazim <nazimazhaan17@gmail.com>
2025-01-07 16:30:06 +0530

    cart redux

--- ecom-frontEnd/src/App.jsx
+++ ecom-frontEnd/src/App.jsx
@@ -5,19 +5,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/shared/NavBar'
 import About from './components/About'
 import Contact from './components/Contact'
+import { Toaster } from 'react-hot-toast'
+import React from 'react'
 
 function App() {
 
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/products' element={<Products />}></Route>
-        <Route path='/about' element={<About />}></Route>
-        <Route path='/contact' element={<Contact />}></Route>
-      </Routes>
-    </Router>
+    <React.Fragment>
+      <Toaster position='top-center' />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/products' element={<Products />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+        </Routes>
+      </Router>
+    </React.Fragment>
   )
 }
 
--- ecom-frontEnd/src/components/shared/ProductCard.jsx
+++ ecom-frontEnd/src/components/shared/ProductCard.jsx
@@ -2,6 +2,10 @@ import { useState } from "react"
 import { FaShoppingCart } from "react-icons/fa";
 import ProductViewModal from "../shared/ProductViewModal";
 import { TruncateText } from "../../utils/TruncateText";
+import About from "../About";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../store/action";
+import toast from "react-hot-toast";
 
 const ProductCard = (props) => {
     const { productId,productName,image,description
@@ -11,11 +15,15 @@ const ProductCard = (props) => {
     const[selectedViewProduct , setSelectedViewProduct] = useState("");
     const btnLoader = false;
     const isAvailable = quantity && Number(quantity) > 0;
+    const dispatch = useDispatch();
 
-    const handleProductView = (product) => {
+    const handleProductView = (product) => {    
         setSelectedViewProduct(product);
         setOpenProductViewModal(true);
     };
+    const addToCartHandler = (cartItems) => {
+        dispatch(addToCart(cartItems , 1 ,toast));
+    }
 
     return(
         <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
@@ -61,7 +69,13 @@ const ProductCard = (props) => {
                         </span>
                     </div>
                 )}
-                <button className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}
+                <button 
+                    disabled = {!isAvailable || btnLoader}
+                    onClick={() => addToCartHandler({
+                        productId,productName,image,description
+                        ,quantity,price,discount,specialPrice
+                    })}
+                    className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}
                     text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 flex w-36 justify-center`}>
                     <FaShoppingCart className="mr-2"/>
                     {isAvailable ? "Add to Cart" : "Out of stock"}
